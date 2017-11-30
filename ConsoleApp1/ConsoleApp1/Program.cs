ed9aab213057e2e7b7f68b88b733d4bb18d37a7d
renatovrodrigues <renatoo.net@gmail.com>
2017-11-30 16:18:58 -0200

    2 opção

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,15 +14,13 @@ namespace ConsoleApp1
             string x;
             int y;
             double z;
-            char w;
-            x = Console.ReadLine();
-            y = int.Parse(Console.ReadLine());
-            z = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            w = char.Parse(Console.ReadLine());
+            string[] vet = Console.ReadLine().Split(' ');
+            x = vet[0];
+            y = int.Parse(vet[1]);
+            z = double.Parse(vet[2], CultureInfo.InvariantCulture);
             Console.WriteLine(x);
             Console.WriteLine(y);
             Console.WriteLine(z.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine(w);
             Console.ReadLine();
         }
     }
